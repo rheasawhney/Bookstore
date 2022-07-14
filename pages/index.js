a128708dfa4c531f853be30c6b9848889a43c0cb
saifmohammed888 <saifmohammed888@gmail.com>
2022-07-14 22:04:50 +0530

    ssd

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Category } from '../components/categories'
 import { FeaturedContainer } from '../components/featured/index'
-import Banner from '../components/common/breadCrum/index'
+import Banner from '../components/home/banner/index'
 import Newsletter from '../components/common/newsletter'
 import Authors from '../components/common/authors'
 import UserLayout from '../components/common/layout'
