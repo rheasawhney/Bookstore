f1991dab6a4b15ef79456612ca4e109cf38bbfe6
saifmohammed888 <saifmohammed888@gmail.com>
2022-08-27 22:00:00 +0530

    Color

--- pages/orderCompletion.js
+++ pages/orderCompletion.js
@@ -8,7 +8,7 @@ const OrderCompletion = () => {
       <Navbar/>
         <section className='w-100 h-[50vh] '>
             <section className='w-[50%] rounded-xl shadow-2xl h-[40vh] border-2 mx-auto mt-10  align-middle flex justify-center'>
-                <p className='text-green-800 text-2xl mt-40 font-bold text-center'>You order is complete, will be delivered<br/> to you by monday!!!</p>
+                <p className='text-violet-800 text-2xl mt-40 font-bold text-center'>You order is complete, will be delivered<br/> to you by monday!!!</p>
             </section>
         </section>
       <Footer/>
